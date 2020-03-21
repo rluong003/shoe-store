1641417ec4b5a1f0415eba20a50bf17dfb3d5e96
Richard <rluon003@ucr.edu>
2020-03-20 17:15:45 -0700

    added redux persist and some production build optimization

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store';
+import { store, persistor } from "./redux/store.js";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/pages/item-page/item-page.jsx
+++ src/pages/item-page/item-page.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { selectItem } from "../../redux/shop/shop.selectors";
+import Button from "@material-ui/core/Button";
 
+import { addItem } from "../../redux/cart/cart.actions";
 import "./item-page.styles.scss";
-function ItemPage({ item }) {
+function ItemPage({ item, addItem }) {
   console.log(item);
   return (
     <div className='product'>
@@ -13,6 +15,13 @@ function ItemPage({ item }) {
       <img src={item.imageUrl} />
       <div className="product-options">
       <h2> {item.name} </h2>
+      <Button
+        onClick={() => addItem(item)}
+        className="custom-button"
+        variant="contained"
+      >
+        ADD TO CART
+      </Button>
       </div>
     </div>
   );
@@ -22,4 +31,10 @@ const mapStateToProps = (state, ownProps) => ({
   item: selectItem(ownProps.match.params.itemId)(state)
 });
 
-export default connect(mapStateToProps)(ItemPage);
+
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemPage);
